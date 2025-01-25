515856209c8bacff2cb3acbe4ae21158cd3c1a65
Praccho <pracchommc@gmail.com>
2025-01-25 15:56:09 -0500

    diagnostics

--- init.lua
+++ init.lua
@@ -163,6 +163,14 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Make sure diagnostics work
+vim.diagnostic.config {
+  virtual_text = true, -- Show inline text at the end of the line
+  signs = true, -- Show signs in the gutter
+  underline = true, -- Underline the problematic code
+  severity_sort = true, -- Sort diagnostics by severity
+}
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
